286437b29c42a370558b583cd626d4a347f1f1d1
dev870 <developershmail@gmail.com>
2021-07-08 23:31:58 +0530

    Add put stream and list stream API

---
+++ server/src/main.rs
@@ -0,0 +1,31 @@
+use actix_web::{put, get, App, HttpRequest, HttpResponse, HttpServer};
+mod stream;
+
+#[put("/{stream}")]
+async fn put_stream(req: HttpRequest) -> HttpResponse {
+    let stream_name: String = req.match_info().get("stream").unwrap().parse().unwrap();
+    let stream_name_clone = stream_name.clone();
+    match stream::insert_stream(stream_name, stream::Stream::empty()) {
+        None => HttpResponse::Ok().body(format!("Created Stream {}", stream_name_clone)),
+        Some(_) =>   {
+            HttpResponse::Ok().body(format!("Updated Stream {}", stream_name_clone))
+        } 
+    }
+}
+
+#[get("/list")]
+async fn list_stream() -> HttpResponse {
+    let map = stream::STREAMS.lock().unwrap();
+    for  (k, _) in map.iter() {
+        println!("key={}", k);
+    }
+    HttpResponse::Ok().body("Listed Stream")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(put_stream).service(list_stream))
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
