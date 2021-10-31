68d8008c22c6edbf72fb8368c1570f86d0d9e753
AdheipSingh <34169002+AdheipSingh@users.noreply.github.com>
2021-10-31 17:52:57 +0530

    improve s3 client init, intro traits (#10)

--- server/src/config.rs
+++ server/src/config.rs
@@ -6,11 +6,11 @@ use http::{Uri};
 
 #[derive(Debug, Deserialize)]
 pub struct ConfigToml {
-    s3: S3,
+    pub s3: S3,
 }
 
 #[derive(Debug,Deserialize)]
-struct S3 {
+pub struct S3 {
     aws_access_key_id: String,
     aws_secret_key:  String,
     aws_default_region:  String,
@@ -19,25 +19,34 @@ struct S3 {
 }
 
 impl ConfigToml {
-    fn new() -> Result<Self, ConfigError> {
+    pub fn new() -> ConfigToml {
+        let c = ConfigToml::new_config();
+        return c.unwrap();
+    }
+    pub fn new_config() -> Result<ConfigToml, ConfigError> {
         let mut s = Config::default();
         s.merge(File::with_name("Config"))?;
         s.try_into()
     }
+}
 
-    pub fn s3client() -> aws_sdk_s3::Client {
+pub trait Aws {
+    fn s3client(&self) -> aws_sdk_s3::Client;
+}
+
+impl Aws for S3 {
+    fn s3client(&self) -> aws_sdk_s3::Client {
         let ( secret_key, access_key, region, endpoint_url, bucket_name ) = ("AWS_SECRET_ACCESS_KEY", "AWS_ACCESS_KEY_ID", "AWS_DEFAULT_REGION", "AWS_ENDPOINT_URL", "AWS_BUCKET_NAME");
         let  data = vec![secret_key, access_key, region, endpoint_url, bucket_name];
-    
+
         for data in data.iter() {
-            let config_info = ConfigToml::new();
             match data {
-                &"AWS_SECRET_ACCESS_KEY" => env::set_var(secret_key, config_info.unwrap().s3.aws_secret_key),
-                &"AWS_ACCESS_KEY_ID" => env::set_var(access_key, config_info.unwrap().s3.aws_access_key_id),
-                &"AWS_DEFAULT_REGION" => env::set_var(region, config_info.unwrap().s3.aws_default_region),
-                &"AWS_ENDPOINT_URL" => env::set_var(endpoint_url, config_info.unwrap().s3.aws_endpoint_url),
-                &"AWS_BUCKET_NAME" => env::set_var(bucket_name, config_info.unwrap().s3.aws_bucket_name),
-                _ => println!("{:?}",config_info),
+                &"AWS_SECRET_ACCESS_KEY" => env::set_var(secret_key, &self.aws_secret_key),
+                &"AWS_ACCESS_KEY_ID" => env::set_var(access_key, &self.aws_access_key_id),
+                &"AWS_DEFAULT_REGION" => env::set_var(region, &self.aws_default_region),
+                &"AWS_ENDPOINT_URL" => env::set_var(endpoint_url, &self.aws_endpoint_url),
+                &"AWS_BUCKET_NAME" => env::set_var(bucket_name, &self.aws_bucket_name),
+                _ => println!("{:?}",&self),
             }
             
         }
