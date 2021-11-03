72a9769a700e3f15bb57c820ff5acf20b6b357d1
AdheipSingh <34169002+AdheipSingh@users.noreply.github.com>
2021-11-03 16:54:01 +0530

    add schema validation (#12)

--- server/src/handler.rs
+++ server/src/handler.rs
@@ -31,12 +31,7 @@ pub async fn post_event(req: HttpRequest, body: web::Json<serde_json::Value>) ->
             // If the schema is empty, this is the first event in this stream. 
             // Parse the arrow schema, upload it to <bucket>/<stream_prefix>/.schema file
             if schema.len() == 0 {
-                let str_body = format!("{}", body);
-                let reader = str_body.as_bytes();
-                let mut buf_reader = BufReader::new(reader);
-                let inferred_schema = infer_json_schema(&mut buf_reader, None).unwrap();  
-                let str_inferred_schema = format!("{}", serde_json::to_string(&inferred_schema).unwrap());
-    
+               let str_inferred_schema = infer_schema(body);
                 match put_schema(&s3_client, &stream_name, str_inferred_schema) {
                     Ok(_) => HttpResponse::Ok().body(format!("Uploading event to Stream {} ", stream_name)),
                     Err(_) => HttpResponse::Ok().body(format!("Stream {} doesn't exist", stream_name))
@@ -52,6 +47,10 @@ pub async fn post_event(req: HttpRequest, body: web::Json<serde_json::Value>) ->
                 // 2. Add the event to existing Arrow RecordBatch. 
                 // 3. Check if event count threshold is reached, convert record batch to parquet and push to S3.
                 // 4. Init new RecordBatch if previos record batch was pushed to S3.
+                let str_inferred_schema = infer_schema(body);
+                if schema != str_inferred_schema.as_bytes() {
+                    return  HttpResponse::Ok().body(format!("Invalid schema for {} ", stream_name))
+                }
                 HttpResponse::Ok().body(format!("Schema already present for Stream {} ", stream_name))
             }
         },
@@ -94,3 +93,12 @@ pub async fn stream_exists(s3_client: &aws_sdk_s3::Client, stream_name: &String)
     let body_bytes = body.unwrap().into_bytes();
     Ok(body_bytes)
 }
+
+fn infer_schema(body: web::Json<serde_json::Value>) -> std::string::String {
+    let str_body = format!("{}", body);
+    let reader = str_body.as_bytes();
+    let mut buf_reader = BufReader::new(reader);
+    let inferred_schema = infer_json_schema(&mut buf_reader, None).unwrap();  
+    let  str_inferred_schema = format!("{}", serde_json::to_string(&inferred_schema).unwrap());
+    return str_inferred_schema
+}   
