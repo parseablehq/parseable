21b0be05ae2fef36c6d0ae5601d39505ac684194
dev870 <61350231+dev870@users.noreply.github.com>
2021-11-18 19:42:59 +0530

    Remove config and use env vars instead. (#13)
    * Remove config and use env vars instead.
    
    Also move around some of the s3 client to make flow simpler
    
    * Add server startup banner
    
    * Improve server startup banner further
    
    * Add server cpu details
    
    * Separate banner functions
    
    * Add basic auth and logging

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,13 @@
-use actix_web::{web, App, HttpServer};
-#[macro_use]
-extern crate serde_derive;
-
+use actix_web::{middleware, web, App, HttpServer, Error};
+use actix_web::dev::ServiceRequest;
+use actix_web_httpauth::extractors::basic::BasicAuth;
+use actix_web_httpauth::middleware::HttpAuthentication;
+use env_logger;
 
 mod handler;                                             
-mod config;
+mod option;
+mod storage;
+mod banner;
 
 // Init
 // Read S3
@@ -14,12 +17,29 @@ mod config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    banner::print();
+    env_logger::init();
+
+    let opt = option::get_opts();
     HttpServer::new(|| {
+        let auth = HttpAuthentication::basic(validator);
+        // match opt.master_key {
+        //     Some(key) => auth = HttpAuthentication::basic(validator),
+        //     _ => auth = nu,
+        // }
         App::new()
+            .wrap(middleware::Logger::default())
+            .wrap(auth)
             .route("/{stream}", web::post().to(handler::post_event))
             .route("/{stream}", web::put().to(handler::put_stream))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(opt.http_addr)?
     .run()
     .await
 }
+
+async fn validator(req: ServiceRequest, _credentials: BasicAuth) -> Result<ServiceRequest, Error> {
+    // pass through for now
+    Ok(req)
+}
+
