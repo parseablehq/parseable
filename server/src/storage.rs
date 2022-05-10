dfce3b9946db179661b30e8ad061dc8e669b650b
Nitish Tiwari <5156139+nitisht@users.noreply.github.com>
2022-05-10 23:33:02 +0530

    Add github ci to build docker image per PR (#106)

--- server/src/storage.rs
+++ server/src/storage.rs
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 use aws_sdk_s3::{Client, Credentials, Endpoint, Error, Region};
