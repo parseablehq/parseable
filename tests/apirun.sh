7851394af7520051f173d957689818b52be5d094
Rishabh Karn <rishabhranjankarn@gmail.com>
2022-05-19 11:35:26 +0530

    Updated apirun.sh to use input.json file for POST stream. (#115)

--- tests/apirun.sh
+++ tests/apirun.sh
@@ -13,19 +13,21 @@ sleep 1
 echo Creating input.json file with 50 events
 ./flog -f json -n 50 -t log -o input.json
 
-#Preparing newinput.json to be used as JSON request for POST:
-echo Creating newinput.json file to be used as raw data for POST
-sed '1s/^/[/;$!s/$/,/;$s/$/]/' input.json > newinput.json
+sleep 2
 
+#Preparing newinput.json to be used as JSON request for POST:
+echo Preparing input.json file to be used as raw data for POST
+sed -i '1s/^/[/;$!s/$/,/;$s/$/]/' input.json
 
 sleep 2
 
+content=$(cat input.json)
 echo Posting 50 events into $2
 #Put 50 events in stream using input.json file
 PUTSTREAM=$(curl --location --request POST $1/api/v1/logstream/$2 \
 --header 'Authorization: Basic YmQ5MTA1MDEtYzg4NC00MDcyLWI5YjctZjk5ZjQ5NGVkZmU0OjRhODZmNWQyLTM1ZDgtNDMwZi1hODdmLTMwOTU5ZDA1YTk5Mg==' \
 --header 'Content-Type: application/json' \
--d "@newinput.json"
+--data-raw "$content"
 )
 echo $PUTSTREAM
 
@@ -47,8 +49,9 @@ QUERYSTREAM=$(curl --location --request GET $1/api/v1/query \
 --data-raw '{
 "query": "select count(*) from '$2'"
 }')
-echo Number of events stored in $2: $QUERYSTREAM
-if [[ $QUERYSTREAM == 50 ]]
+queryResult=$(echo $QUERYSTREAM | cut -d ':' -f2 | cut -d '}' -f1)
+echo Number of events stored in $2: $queryResult
+if [ $queryResult = 50 ]
 then
   echo "Validation successful. Count of events returned from query is same as the ones posted."
 else
@@ -57,5 +60,5 @@ fi
 sleep 3
 
 #Delete the input.json file
-echo deleting the input.json and newinput.json files
-rm -rf input.json newinput.json
+echo deleting the input.json file
+rm -rf input.json
