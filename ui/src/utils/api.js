3c2a20b2027318f221668059f2be87e40c138ddc
Nitish Tiwari <tiwari.nitish@gmail.com>
2022-07-31 22:10:54 +0530

    Remove localhost URL (#224)

--- ui/src/utils/api.js
+++ ui/src/utils/api.js
@@ -1,12 +1,3 @@
-export const LOCAL_SERVER_URL = "http://localhost:5678/";
-
 export const getServerURL = () => {
-  if (
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-  ) {
-    return LOCAL_SERVER_URL;
-  } else {
-    return "/";
-  }
+  return "/";
 };
