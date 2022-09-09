31273c28f5e45b6a4f70db9b53527b1a113f34e4
Satyam Singh <trueleo@protonmail.com>
2022-09-09 19:25:55 +0530

    Append arrow directly to local data (#100)
    Currently any event that arrives is directly written to 
    data.parquet. It is easier to do it this way but this 
    ultimately slows down log ingestion.To make log 
    ingestion faster a stream of record-batches would 
    act as an ideal buffer. 
    
    Appending arrow record-batches directly to a stream 
    is faster way to go about it but it has challenges:
    
    - querying realtime would require having these 
    record-batches in memory.
    - directly appending record-batches from multiple 
    threads to an append only file is not supported by 
    Arrow StreamWriter. Locking is used for now.
    - Re-implementation of stats is required
    - Cannot fully guarantee state of local storage if 
    it failed exactly at move step of localsync. orphan
    tmp files can occur although that'd be rare

--- server/src/handlers/logstream.rs
+++ server/src/handlers/logstream.rs
@@ -22,9 +22,9 @@ use actix_web::http::StatusCode;
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
 
 use crate::alerts::Alerts;
-use crate::response;
 use crate::s3::S3;
 use crate::storage::{ObjectStorage, StorageDir};
+use crate::{event, response};
 use crate::{metadata, validator};
 
 pub async fn delete(req: HttpRequest) -> HttpResponse {
@@ -59,20 +59,27 @@ pub async fn delete(req: HttpRequest) -> HttpResponse {
         .to_http();
     }
 
-    let stream_dir = StorageDir::new(&stream_name);
-    if fs::remove_dir_all(&stream_dir.data_path).is_err() {
-        log::warn!(
-            "failed to delete local data for stream {}. Clean {} manually",
-            stream_name,
-            stream_dir.data_path.to_string_lossy()
-        )
-    }
-
     if let Err(e) = metadata::STREAM_INFO.delete_stream(&stream_name) {
         log::warn!(
             "failed to delete log stream {} from metadata due to err: {}",
             stream_name,
             e
+        )
+    }
+
+    if event::STREAM_WRITERS::delete_entry(&stream_name).is_err() {
+        log::warn!(
+            "failed to delete log stream event writers for stream {}",
+            stream_name
+        )
+    }
+
+    let stream_dir = StorageDir::new(stream_name.clone());
+    if fs::remove_dir_all(&stream_dir.data_path).is_err() {
+        log::warn!(
+            "failed to delete local data for stream {}. Clean {} manually",
+            stream_name,
+            stream_dir.data_path.to_string_lossy()
         )
     }
 
