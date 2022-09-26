90ddb4ca376efae5ae71efe0344ca03cf40cac21
Satyam Singh <trueleo@protonmail.com>
2022-09-26 13:38:20 +0530

    Update banner to show latest available version
    When stating a Parseable instance it will check for latest release and if the
    currrent version is outdated then additional info is printed on banner about
    latest release along with download link.
    
    Changes
    - Banner will display current verison
    - Banner will display latest version if current version is not latest along
    with download link

--- server/src/banner.rs
+++ server/src/banner.rs
@@ -43,3 +43,60 @@ pub fn warning_line() {
         "Warning:".to_string().red().bold(),
     );
 }
+
+pub mod version {
+    use chrono::Duration;
+    use chrono_humanize::{Accuracy, Tense};
+    use crossterm::style::Stylize;
+    use semver::Version;
+
+    use crate::utils::github;
+
+    pub fn print() {
+        // print current version
+        let current_version = current_version();
+        // not eprintln because if it is old release then time passed with be displayed beside it
+        eprint!(
+            "
+    {} {} ",
+            "Current Version:".to_string().blue().bold(),
+            current_version
+        );
+
+        // check for latest release, if it cannot be fetched then print error as warn and return
+        let latest_release = match github::get_latest() {
+            Ok(latest_release) => latest_release,
+            Err(e) => {
+                log::warn!("{}", e);
+                return;
+            }
+        };
+
+        if latest_release.version > current_version {
+            let time_since_latest_release = chrono::Utc::now() - latest_release.date;
+            let time_since_latest_release = humanize_time(time_since_latest_release);
+
+            let fmt_latest_version = format!(
+                "( v{} released {} ago )",
+                latest_release.version, time_since_latest_release
+            );
+
+            eprint!("{}", fmt_latest_version.yellow().bold());
+            eprintln!(
+                "
+    Download latest version from https://github.com/parseablehq/parseable/releases/latest"
+            );
+        } else {
+            eprintln!();
+        }
+    }
+
+    fn current_version() -> Version {
+        let current_version = env!("CARGO_PKG_VERSION");
+        semver::Version::parse(current_version).expect("CARGO_PKG_VERSION is always valid semver")
+    }
+
+    fn humanize_time(time_passed: Duration) -> String {
+        chrono_humanize::HumanTime::from(time_passed).to_text_en(Accuracy::Rough, Tense::Present)
+    }
+}
