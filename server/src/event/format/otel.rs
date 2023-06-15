f1b18b553ce310ff9a2e3432b03dada16fa4a4f1
Satyam Singh <trueleo@protonmail.com>
2023-06-15 15:01:20 +0530

    Fix

--- server/src/event/format/otel.rs
+++ server/src/event/format/otel.rs
@@ -3,13 +3,5 @@ mod log;
 mod resource;
 mod trace;
 
-pub use log::LogsData;
-pub use trace::TracesData;
-
-impl TryFrom<&TracesData> for serde_json::Value {
-    type Error = serde_json::Error;
-
-    fn try_from(value: &TracesData) -> Result<Self, Self::Error> {
-        serde_json::to_value(value)
-    }
-}
+pub use self::log::LogsData;
+pub use self::trace::TracesData;
