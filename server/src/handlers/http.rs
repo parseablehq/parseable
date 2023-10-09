c10259aa5224ad290ac1d099fab4c5178eadde0e
Aldrin Jenson <aldrinjenson@gmail.com>
2023-10-10 04:18:18 +0530

    revert change for dynami path

--- server/src/handlers/http.rs
+++ server/src/handlers/http.rs
@@ -290,7 +290,7 @@ pub fn configure_routes(
     let external_services = web::scope("modules")
         .service(resource("").route(web::get().to(external::get)))
         .service(
-            resource("/{module}")
+            resource("/register")
                 .route(web::put().to(external::register))
                 .route(web::delete().to(external::deregister)),
         )
