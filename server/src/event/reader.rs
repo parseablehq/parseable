3d96aaaa5c201d673ad90e996853d70f15c0c188
Eshan Chatterjee <eshan2001221@gmail.com>
2024-01-25 15:22:56 +0530

    Concreate implementation for get_staged_records

--- server/src/event/reader.rs
+++ server/src/event/reader.rs
@@ -1,26 +1,48 @@
 use std::sync::Arc;
 
+use arrow_array::RecordBatch;
 use itertools::Itertools;
 
 use crate::{storage::StorageDir, utils::arrow::merged_reader::MergedReverseRecordReader};
 
-#[allow(dead_code)]
-pub fn get_data_from_arrows_file(dir: &StorageDir) -> Result<(), ()> {
-    // I need the files in staging
-    let time = chrono::Utc::now().naive_utc();
-    let staging_files = dir.arrow_files_grouped_exclude_time(time);
+/// Reads data from an Arrow file.
+///
+/// This function takes a shared reference to `StorageDir` struct and reads all the data from
+/// the Arrows files located at that path. With a MergedReverseRecordReader
+/// It returns the data as a `RecordBatch`.
+///
+/// # Arguments
+///
+/// * `&dir` - A shared reference to `StorageDir` struct .
+///
+/// # Returns
+///
+/// A `Result<RecordBatch, &str>` representing the data read from the Arrow file. Returns an `ArrowError` if the file could not be read.
+///
+/// # Example
+///
+/// ```
+/// let dir = StorageDir::new("stream_name");
+/// let data = get_staged_records(dir);
+/// match data {
+///     Ok(record_batch) => println!("Data: {:?}", record_batch),
+///     Err(e) => println!("Error reading data: {:?}", e),
+/// }
+/// ```
+///
+/// See [StorageDir](server/src/storage/staging.rs) for more information about StorageDir struct
+/// See [MergedReverseRecordReader](server/src/utils/arrow/merged_reader.rs) for more information about MergedReverseRecordReader struct
+/// This function is defined in [server/src/reader.rs](server/src/reader.rs).
+pub fn get_staged_records(dir: &StorageDir) -> Result<Vec<RecordBatch>, &'static str> {
+    let staging_files = dir.arrow_files();
 
-    for (_, files) in staging_files {
-        let record_reader = MergedReverseRecordReader::try_new(&files)?;
+    let record_reader = match MergedReverseRecordReader::try_new(&staging_files) {
+        Ok(mrbr) => mrbr,
+        Err(_) => {
+            return Err("Cannot Get Merged Iterator");
+        }
+    };
 
-        let schema = Arc::new(record_reader.merged_schema());
-
-        let record_batches = record_reader.merged_iter(schema).collect_vec();
-
-        dbg!(record_batches.len());
-        // for ref some in record_reader.merged_iter(schema) {
-        //     dbg!("{:?}", some);
-        // }
-    }
-    Ok(())
+    let schema = Arc::new(record_reader.merged_schema());
+    Ok(record_reader.merged_iter(schema).collect_vec())
 }
