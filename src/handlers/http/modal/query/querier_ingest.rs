1eaf769bbf1a2b6910c30995b447c8095c6feec7
Devdutt Shenoi <devdutt@parseable.com>
2025-01-15 13:55:40 +0530

    don't extract where not required

--- src/handlers/http/modal/query/querier_ingest.rs
+++ src/handlers/http/modal/query/querier_ingest.rs
@@ -16,14 +16,14 @@
  *
  */
 
+use actix_web::HttpResponse;
+
 use crate::handlers::http::ingest::PostError;
-use actix_web::{HttpRequest, HttpResponse};
-use bytes::Bytes;
 
 // Handler for POST /api/v1/logstream/{logstream}
 // only ingests events into the specified logstream
 // fails if the logstream does not exist
 #[allow(unused)]
-pub async fn post_event(req: HttpRequest, body: Bytes) -> Result<HttpResponse, PostError> {
+pub async fn post_event() -> Result<HttpResponse, PostError> {
     Err(PostError::IngestionNotAllowed)
 }
