3cee27198c95da8a98229ff8bc245239627ddfb0
Devdutt Shenoi <devdutt@parseable.com>
2025-02-18 13:12:36 +0530

    refactor: directly construct `Field`

--- src/static_schema.rs
+++ src/static_schema.rs
@@ -16,12 +16,14 @@
  *
  */
 
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 
 use arrow_schema::{DataType, Field, Schema, TimeUnit};
 use serde::{Deserialize, Serialize};
 
 use crate::event::DEFAULT_TIMESTAMP_KEY;
+
+const DEFAULT_NULLABLE: bool = true;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct StaticSchema {
@@ -48,7 +50,7 @@ impl StaticSchema {
         time_partition: &str,
         custom_partition: Option<&String>,
     ) -> Result<Arc<Schema>, StaticSchemaError> {
-        let mut fields = Vec::new();
+        let mut schema: Vec<Arc<Field>> = Vec::new();
         let mut time_partition_exists = false;
 
         if let Some(custom_partition) = custom_partition {
@@ -68,48 +70,28 @@ impl StaticSchema {
                 field.data_type = "datetime".to_string();
             }
 
-            let parsed_field = Fields {
-                name: field.name.clone(),
-
-                data_type: {
-                    match field.data_type.as_str() {
-                        "int" => DataType::Int64,
-                        "double" | "float" => DataType::Float64,
-                        "boolean" => DataType::Boolean,
-                        "string" => DataType::Utf8,
-                        "datetime" => DataType::Timestamp(TimeUnit::Millisecond, None),
-                        "string_list" => {
-                            DataType::List(Arc::new(Field::new("item", DataType::Utf8, true)))
-                        }
-                        "int_list" => {
-                            DataType::List(Arc::new(Field::new("item", DataType::Int64, true)))
-                        }
-                        "double_list" | "float_list" => {
-                            DataType::List(Arc::new(Field::new("item", DataType::Float64, true)))
-                        }
-                        "boolean_list" => {
-                            DataType::List(Arc::new(Field::new("item", DataType::Boolean, true)))
-                        }
-                        _ => DataType::Null,
-                    }
-                },
-                nullable: default_nullable(),
-                dict_id: default_dict_id(),
-                dict_is_ordered: default_dict_is_ordered(),
-                metadata: HashMap::new(),
+            let data_type = match field.data_type.as_str() {
+                "int" => DataType::Int64,
+                "double" | "float" => DataType::Float64,
+                "boolean" => DataType::Boolean,
+                "string" => DataType::Utf8,
+                "datetime" => DataType::Timestamp(TimeUnit::Millisecond, None),
+                "string_list" => DataType::List(Arc::new(Field::new("item", DataType::Utf8, true))),
+                "int_list" => DataType::List(Arc::new(Field::new("item", DataType::Int64, true))),
+                "double_list" | "float_list" => {
+                    DataType::List(Arc::new(Field::new("item", DataType::Float64, true)))
+                }
+                "boolean_list" => {
+                    DataType::List(Arc::new(Field::new("item", DataType::Boolean, true)))
+                }
+                _ => DataType::Null,
             };
-
-            fields.push(parsed_field);
+            let field = Field::new(&field.name, data_type, DEFAULT_NULLABLE);
+            schema.push(Arc::new(field));
         }
 
         if !time_partition.is_empty() && !time_partition_exists {
             return Err(StaticSchemaError::MissingTime(time_partition.to_owned()));
-        }
-
-        let mut schema: Vec<Arc<Field>> = Vec::new();
-        for field in fields {
-            let field = Field::new(field.name.clone(), field.data_type.clone(), field.nullable);
-            schema.push(Arc::new(field));
         }
 
         // add the p_timestamp field to the event schema to the 0th index
@@ -131,29 +113,4 @@ impl StaticSchema {
 pub struct SchemaFields {
     name: String,
     data_type: String,
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Fields {
-    name: String,
-    data_type: DataType,
-    nullable: bool,
-    dict_id: i64,
-    dict_is_ordered: bool,
-    metadata: HashMap<String, String>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-
-pub struct Metadata {}
-
-fn default_nullable() -> bool {
-    true
-}
-fn default_dict_id() -> i64 {
-    0
-}
-fn default_dict_is_ordered() -> bool {
-    false
 }
