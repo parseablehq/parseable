33ed89cc181d083c4ce122b76387b0cc7b8cd3d5
parmesant <anant.v09@proton.me>
2025-07-04 13:56:40 +0530

    fix: fix up issues in target separation (#1365)
    - Removal of duplication check
    - Proper loading of targets on server init
    - Masking of sensitive information (slack URL, alert manager auth credentials)
    - Error on modifying name
    - Renaming repeat to notificationConfig and setting least count of interval to 1 minute

--- src/handlers/http/targets.rs
+++ src/handlers/http/targets.rs
@@ -2,6 +2,7 @@ use actix_web::{
     web::{self, Json, Path},
     HttpRequest, Responder,
 };
+use itertools::Itertools;
 use ulid::Ulid;
 
 use crate::alerts::{
@@ -18,13 +19,18 @@ pub async fn post(
     // add to the map
     TARGETS.update(target.clone()).await?;
 
-    Ok(web::Json(target))
+    Ok(web::Json(target.mask()))
 }
 
 // GET /targets
 pub async fn list(_req: HttpRequest) -> Result<impl Responder, AlertError> {
     // add to the map
-    let list = TARGETS.list().await?;
+    let list = TARGETS
+        .list()
+        .await?
+        .into_iter()
+        .map(|t| t.mask())
+        .collect_vec();
 
     Ok(web::Json(list))
 }
@@ -35,7 +41,7 @@ pub async fn get(_req: HttpRequest, target_id: Path<Ulid>) -> Result<impl Respon
 
     let target = TARGETS.get_target_by_id(&target_id).await?;
 
-    Ok(web::Json(target))
+    Ok(web::Json(target.mask()))
 }
 
 // PUT /targets/{target_id}
@@ -47,7 +53,14 @@ pub async fn update(
     let target_id = target_id.into_inner();
 
     // if target_id does not exist, error
-    TARGETS.get_target_by_id(&target_id).await?;
+    let old_target = TARGETS.get_target_by_id(&target_id).await?;
+
+    // do not allow modifying name
+    if old_target.name != target.name {
+        return Err(AlertError::InvalidTargetModification(
+            "Can't modify target name".to_string(),
+        ));
+    }
 
     // esnure that the supplied target id is assigned to the target config
     target.id = target_id;
@@ -56,7 +69,7 @@ pub async fn update(
     // add to the map
     TARGETS.update(target.clone()).await?;
 
-    Ok(web::Json(target))
+    Ok(web::Json(target.mask()))
 }
 
 // DELETE /targets/{target_id}
@@ -68,5 +81,5 @@ pub async fn delete(
 
     let target = TARGETS.delete(&target_id).await?;
 
-    Ok(web::Json(target))
+    Ok(web::Json(target.mask()))
 }
