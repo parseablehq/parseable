16189ebe413f8f7339e97ebbf53a68c507d80c81
Nikhil Sinha <131262146+nikhilsinhaparseable@users.noreply.github.com>
2025-11-17 10:40:59 -0800

    store mttr history for given date (#1451)
    * store mttr history for given date
    
    add structs for daily mttr history
    add helper functions in object store to save mttr history
    
    * update mttr path

--- src/metastore/metastores/object_store_metastore.rs
+++ src/metastore/metastores/object_store_metastore.rs
@@ -32,7 +32,10 @@ use tracing::warn;
 use ulid::Ulid;
 
 use crate::{
-    alerts::{alert_structs::AlertStateEntry, target::Target},
+    alerts::{
+        alert_structs::{AlertStateEntry, MTTRHistory},
+        target::Target,
+    },
     catalog::{manifest::Manifest, partition_path},
     handlers::http::{
         modal::{Metadata, NodeMetadata, NodeType},
@@ -49,7 +52,7 @@ use crate::{
         SETTINGS_ROOT_DIRECTORY, STREAM_METADATA_FILE_NAME, STREAM_ROOT_DIRECTORY,
         TARGETS_ROOT_DIRECTORY,
         object_storage::{
-            alert_json_path, alert_state_json_path, filter_path, manifest_path,
+            alert_json_path, alert_state_json_path, filter_path, manifest_path, mttr_json_path,
             parseable_json_path, schema_path, stream_json_path, to_bytes,
         },
     },
@@ -303,6 +306,28 @@ impl Metastore for ObjectStoreMetastore {
             .storage
             .delete_object(&RelativePathBuf::from(path))
             .await?)
+    }
+
+    /// Get MTTR history from storage
+    async fn get_mttr_history(&self) -> Result<Option<MTTRHistory>, MetastoreError> {
+        let path = mttr_json_path();
+        match self.storage.get_object(&path).await {
+            Ok(bytes) => {
+                if let Ok(history) = serde_json::from_slice::<MTTRHistory>(&bytes) {
+                    Ok(Some(history))
+                } else {
+                    Ok(None)
+                }
+            }
+            Err(ObjectStorageError::NoSuchKey(_)) => Ok(None),
+            Err(e) => Err(MetastoreError::ObjectStorageError(e)),
+        }
+    }
+
+    /// Put MTTR history to storage
+    async fn put_mttr_history(&self, obj: &dyn MetastoreObject) -> Result<(), MetastoreError> {
+        let path = RelativePathBuf::from(obj.get_object_path());
+        Ok(self.storage.put_object(&path, to_bytes(obj)).await?)
     }
 
     /// This function fetches all the llmconfigs from the underlying object store
