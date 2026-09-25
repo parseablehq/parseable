2e1b5ab1792abc7abb2358a3ad510f72ed46ebb2
Nikhil Sinha <nikhil.sinha@cloudsurfex.com>
2026-09-25 15:52:12 +0530

    custom buckets for conversion and query execution time

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -34,6 +34,14 @@ use prometheus::{
 
 pub const METRICS_NAMESPACE: &str = env!("CARGO_PKG_NAME");
 
+const SYNC_TASK_DURATION_BUCKETS: &[f64] = &[
+    15.0, 30.0, 45.0, 60.0, 90.0, 120.0, 150.0, 180.0, 300.0, 600.0,
+];
+
+const QUERY_EXECUTE_TIME_BUCKETS: &[f64] = &[
+    0.1, 0.2, 0.5, 1.0, 5.0, 15.0, 30.0, 45.0, 60.0, 90.0, 120.0, 150.0, 180.0,
+];
+
 pub static METRICS_REGISTRY: Lazy<Registry> = Lazy::new(|| {
     let registry = Registry::new();
     custom_metrics(&registry);
@@ -294,7 +302,9 @@ mod process_metrics_tests {
 
 pub static QUERY_EXECUTE_TIME: Lazy<HistogramVec> = Lazy::new(|| {
     HistogramVec::new(
-        HistogramOpts::new("query_execute_time", "Query execute time").namespace(METRICS_NAMESPACE),
+        HistogramOpts::new("query_execute_time", "Query execute time")
+            .namespace(METRICS_NAMESPACE)
+            .buckets(QUERY_EXECUTE_TIME_BUCKETS.to_vec()),
         &["stream", "tenant_id"],
     )
     .expect("metric can be created")
@@ -306,7 +316,8 @@ pub static CONVERSION_TASK_DURATION: Lazy<HistogramVec> = Lazy::new(|| {
             "conversion_task_duration",
             "Local conversion task duration in seconds",
         )
-        .namespace(METRICS_NAMESPACE),
+        .namespace(METRICS_NAMESPACE)
+        .buckets(SYNC_TASK_DURATION_BUCKETS.to_vec()),
         &["phase"],
     )
     .expect("metric can be created")
@@ -318,7 +329,8 @@ pub static OBJECT_STORE_SYNC_DURATION: Lazy<HistogramVec> = Lazy::new(|| {
             "object_store_sync_duration",
             "Object store sync duration in seconds",
         )
-        .namespace(METRICS_NAMESPACE),
+        .namespace(METRICS_NAMESPACE)
+        .buckets(SYNC_TASK_DURATION_BUCKETS.to_vec()),
         &["phase"],
     )
     .expect("metric can be created")
